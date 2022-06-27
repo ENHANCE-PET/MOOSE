3727ed991cc529c15c633a86ed5cef2a33f005ec
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2022-06-27 17:12:38 +0200

    Fixed moose_uninstaller.sh to remove env variables Now we use 'sed' instead of 'unset' to remove the env variables related to 'moose' during the uninstall process.
    Signed-off-by: nutellaBear <48599863+LalithShiyam@users.noreply.github.com>

--- moose_uninstaller.sh
+++ moose_uninstaller.sh
@@ -36,9 +36,10 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     BRAIN_DIR="BRAIN_DETECTOR_DIR"
     MOOSE_DIR="MOOSE_DIR"
     BASH_RC_PATH="$HOME/.bashrc"
-    sed "/${NNUNET_DIR}/d" "${BASH_RC_PATH}"
-    sed "/${RESULTS_DIR}/d" "${BASH_RC_PATH}"
-    sed "/${SIM_SPACE_DIR}/d" "${BASH_RC_PATH}"
-    sed "/${BRAIN_DIR}/d" "${BASH_RC_PATH}"
-    sed "/${MOOSE_DIR}/d" "${BASH_RC_PATH}"
+    sed -i "/${NNUNET_DIR}/d" "${BASH_RC_PATH}"
+    sed -i "/${RESULTS_DIR}/d" "${BASH_RC_PATH}"
+    sed -i "/${SIM_SPACE_DIR}/d" "${BASH_RC_PATH}"
+    sed -i "/${BRAIN_DIR}/d" "${BASH_RC_PATH}"
+    sed -i "/${MOOSE_DIR}/d" "${BASH_RC_PATH}"
 fi
+
