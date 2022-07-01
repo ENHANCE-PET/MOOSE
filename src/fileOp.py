3e9f2e65625b7f83de8443b2727302f796377223
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2022-07-01 10:15:14 +0200

    Update fileOp.py

--- src/fileOp.py
+++ src/fileOp.py
@@ -37,7 +37,8 @@ def display_citation():
     Display manuscript citation
     :return:
     """
-    print("Citation:")
+    print("CITATION:")
+    print(" ")
     print(" Fully-automated, semantic segmentation of whole-body 18F-FDG PET/CT images based on data-centric artificial intelligence")
     print(" Lalith Kumar Shiyam Sundar, Josef Yu, Otto Muzik, Oana Kulterer, Barbara J Fueger, Daria Kifjak, Thomas Nakuz, Hiyung Min Shin, Annika Katharina Sima, Daniela Kitzmantl, Ramsey Derek Badawi, Lorenzo Nardo, Simon R. Cherry, Benjamin A. Spencer, Marcus Hacker, Thomas Beyer")
     print(" Journal of Nuclear Medicine Jun 2022, jnumed.122.264063; DOI: 10.2967/jnumed.122.264063")
