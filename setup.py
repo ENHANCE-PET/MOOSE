ed11830e095b814442865c5ecd8e3136db2add1a
LalithShiyam <lalith.shiyamsundar@meduniwien.ac.at>
2023-02-09 10:39:52 +0100

    MOOSE is now mooseZ
    Ground up build of the second version of MOOSE

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mooseZ',
+    version='2.0.0',
+    author='Lalith Kumar Shiyam Sundar',
+    author_email='Lalith.shiyamsundar@meduniwien.ac.at',
+    description='An AI-inference engine for 3D clinical and preclinical whole-body segmentation tasks',
+    long_description='mooseZ is an AI-inference engine based on nnUNet, designed for 3D clinical and preclinical'
+                     ' whole-body segmentation tasks. It serves models tailored towards different modalities such'
+                     ' as PET, CT, and MR. mooseZ provides fast and accurate segmentation results, making it a '
+                     'reliable tool for medical imaging applications.',
+    url='https://github.com/QIMP-Team/mooseZ',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Researchers',
+        'Intended Audience :: Healthcare Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    ],
+    keywords='ai-inference engine 3d-segmentation medical-imaging',
+    packages=find_packages(),
+    install_requires=[
+        'nibabel~=3.2.2',
+        'halo~=0.0.31',
+        'pandas~=1.4.1',
+        'SimpleITK~=2.1.1',
+        'pydicom~=2.2.2',
+        'argparse~=1.4.0',
+        'numpy~=1.22.3',
+        'mpire~=2.3.3',
+        'openpyxl~=3.0.9',
+        'matplotlib~=3.1.3',
+        'pyfiglet~=0.8.post1',
+        'natsort~=8.1.0',
+        'pillow>=9.2.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mooseZ=mooseZ.mooseZ:main',
+        ],
+    },
+)
