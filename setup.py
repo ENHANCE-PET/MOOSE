136cdbf8125251c9c6a9d6576ad048d193c4e47e
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2023-06-12 18:28:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='moosez',
-    version='2.0.7',
+    version='2.0.8',
     author='Lalith Kumar Shiyam Sundar',
     author_email='Lalith.shiyamsundar@meduniwien.ac.at',
     description='An AI-inference engine for 3D clinical and preclinical whole-body segmentation tasks',
