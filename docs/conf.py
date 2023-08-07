50e26c9f16345e83bc6209256efbdb92d80b0e13
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2023-08-07 21:00:56 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
-#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
 import sys
-from sphinx.application import Sphinx
 
+# Insert the parent directory (where your code is) into the system path
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'MOOSE'
@@ -14,14 +13,15 @@ copyright = '2023, Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer, QIMP'
 author = 'Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer, QIMP'
 release = '2.0'
 
-# -- General configuration ---------------------------------------------------
-
+# Extensions
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx', 
     'sphinx.ext.viewcode',
+    'sphinx.ext.linkcode',  # Add this line for direct linking to GitHub source
 ]
 
+# Intersphinx mapping for external libraries
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'numpy': ('https://numpy.org/doc/stable/', None),
@@ -31,24 +31,24 @@ intersphinx_mapping = {
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output -------------------------------------------------
-
+# Theme and static files
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 
-# GitHub linking
+# GitHub linking for "Edit on Github" feature
 html_context = {
-    "display_github": True,  # Add 'Edit on Github' link instead of 'View page source'
+    "display_github": True,
     "github_user": "LalithShiyam",
     "github_repo": "MOOSE",
     "github_version": "main",
     "conf_py_path": "/docs/",
 }
 
-# Fixing the GitHub source link
-def fix_github_source_link(app: Sphinx, pagename: str, templatename: str, context: dict, doctree):
-    if "viewcode_target" in context:
-        context["viewcode_target"] = context["viewcode_target"].replace("/moosez/moosez/", "/moosez/")
-
-def setup(app: Sphinx):
-    app.connect("html-page-context", fix_github_source_link)
+# Function to resolve direct linking to GitHub source
+def linkcode_resolve(domain, info):
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+    filename = info['module'].replace('.', '/')
+    return f"https://github.com/LalithShiyam/MOOSE/blob/main/{filename}.py"
