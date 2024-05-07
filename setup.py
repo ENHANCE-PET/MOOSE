b574151b39df01922f4835f62e360ea91d0fc8d8
Manuel Pires <48754309+mprires@users.noreply.github.com>
2024-05-07 16:26:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='moosez',
     version='2.5.0',
-    author='Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer',
+    author='Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer | Manuel Pires',
     author_email='Lalith.shiyamsundar@meduniwien.ac.at',
     description='An AI-inference engine for 3D clinical and preclinical whole-body segmentation tasks',
     python_requires='>=3.10',
