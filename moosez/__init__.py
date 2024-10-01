91e3cf8095dfb65d667075f8feeccd252538c7a0
Keyn34 <gutschmayer@gmail.com>
2024-10-01 16:00:31 +0200

    Installation Hotfix
    adjusted __init__.py to rule out installation errors.

--- moosez/__init__.py
+++ moosez/__init__.py
@@ -1,10 +1,3 @@
-from . import constants
-from . import display
-from . import file_utilities
-from . import image_conversion
-from . import input_validation
-from . import moosez
-from . import predict
-from . import resources
 from .moosez import moose
-from .nnUNet_custom_trainer import utility
+
+__all__ = ['moose']
