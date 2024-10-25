fee36c5004bec03f3f367e7f8d620b27efce2692
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2024-10-25 21:09:02 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='moosez',
-    version="3.0.0",
+    version="3.0.1",
     author='Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer | Manuel Pires',
     author_email='Lalith.shiyamsundar@meduniwien.ac.at',
     description='An AI-inference engine for 3D clinical and preclinical whole-body segmentation tasks',
