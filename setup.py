e86b9629f21f1b3de7b63f69d5b448ea4995ddc3
Keyn34 <gutschmayer@gmail.com>
2024-12-02 18:32:08 +0100

    Fixed Python requires in setup.py to >= 3.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer | Manuel Pires',
     author_email='Lalith.shiyamsundar@meduniwien.ac.at',
     description='An AI-inference engine for 3D clinical and preclinical whole-body segmentation tasks',
-    python_requires='>=3.10',
+    python_requires='>=3.9',
     long_description='mooseZ is an AI-inference engine based on nnUNet, designed for 3D clinical and preclinical'
                      ' whole-body segmentation tasks. It serves models tailored towards different modalities such'
                      ' as PET, CT, and MR. mooseZ provides fast and accurate segmentation results, making it a '
