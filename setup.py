865de829aad475d15337d98246b69c3a5b5c1e2c
Keyn34 <gutschmayer@gmail.com>
2025-09-29 12:26:51 +0200

    Removed version restriction of numpy.
    setup.py
    - removed version restriction of numpy.
    - elevated version of moosez to 3.0.31
    
    requirements.txt
    - removed version restriction of numpy and argparse as requirement.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='moosez',
-    version="3.0.30",
+    version="3.0.31",
     author='Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer | Manuel Pires',
     author_email='Lalith.shiyamsundar@meduniwien.ac.at',
     description='An AI-inference engine for 3D clinical and preclinical whole-body segmentation tasks',
@@ -36,7 +36,7 @@ setup(
         'halo',
         'pydicom',
         'argparse',
-        'numpy<2.0',
+        'numpy',
         'pyfiglet',
         'natsort',
         'colorama',
